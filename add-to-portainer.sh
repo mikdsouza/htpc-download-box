d306982129c0bf7efb923e2f1d58a0fd45434853
Mikhail D'Souza <mdsouza@atlassian.com>
2021-01-24 18:04:34 +1100

    Updating some instructions

--- add-to-portainer.sh
+++ add-to-portainer.sh
@@ -1,2 +1,5 @@
+# Located in ./volume1/.@plugins/AppCentral/portainer-ce/CONTROL/start-stop.sh
+# Add this undert the "start" instructions
+
 echo "Kill myhttp to so nginx can take it over"
 /usr/bin/pkill /usr/sbin/myhttpd
