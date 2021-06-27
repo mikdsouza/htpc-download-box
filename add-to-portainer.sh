8f7d41265b9fdba8b2e2e89b4633797abf23ddd4
Mikhail D'Souza <mikdsouza34@gmail.com>
2021-06-28 08:46:27 +1000

    Fixing a typo

--- add-to-portainer.sh
+++ add-to-portainer.sh
@@ -1,5 +1,5 @@
 # Located in ./volume1/.@plugins/AppCentral/portainer-ce/CONTROL/start-stop.sh
-# Add this undert the "start" instructions
+# Add this under the "start" instructions
 
 echo "Kill myhttp to so nginx can take it over"
 /usr/bin/pkill /usr/sbin/myhttpd
